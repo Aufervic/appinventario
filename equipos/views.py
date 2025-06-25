e37395788507ff2c0404cc0ba516689d4a8beb0c
Aufer Victoriano <aufer.vicc@gmail.com>
2025-06-25 15:42:42 -0500

    Maneja autenticacion con JWT token

--- equipos/views.py
+++ equipos/views.py
@@ -4,61 +4,81 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Equipos
 from .serializers import EquiposSerializer, EquiposDetailSerializer
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
 class ListEquiposApiView(APIView):
-    allowed_methods = ['GET', 'POST']
-    def get(self,request):
-        equipos=Equipos.objects.select_related('tipo_ingreso', 'estado', 'ubicacion').all()
-        serializer=EquiposDetailSerializer(equipos,many=True)
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    allowed_methods = ["GET", "POST"]
+
+    def get(self, request):
+        equipos = Equipos.objects.select_related(
+            "tipo_ingreso", "estado", "ubicacion"
+        ).all()
+        serializer = EquiposDetailSerializer(equipos, many=True)
         return Response(serializer.data)
-    def post(self,request):
-        serializer=EquiposSerializer(data=request.data)
+
+    def post(self, request):
+        serializer = EquiposSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
 class DetailEquipos(APIView):
-    def get_object(self,pk):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
         try:
             return Equipos.objects.get(id=pk)
         except Equipos.DoesNotExist:
             return None
-        
-    def get_objectDetail(self,pk):
+
+    def get_objectDetail(self, pk):
         try:
-            return Equipos.objects.select_related('tipo_ingreso', 'estado', 'ubicacion').get(id=pk)
+            return Equipos.objects.select_related(
+                "tipo_ingreso", "estado", "ubicacion"
+            ).get(id=pk)
         except Equipos.DoesNotExist:
             return None
-        
-    def get(self,request,pk):
-        equipo=self.get_object(pk)
+
+    def get(self, request, pk):
+        equipo = self.get_object(pk)
         print(equipo.estado)
         if equipo is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer=EquiposDetailSerializer(equipo)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    def put(self,request,pk):
-        equipo=self.get_object(pk)
+        serializer = EquiposDetailSerializer(equipo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        equipo = self.get_object(pk)
         if equipo is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer=EquiposSerializer(equipo, data=request.data)
+        serializer = EquiposSerializer(equipo, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-    def delete(self,request,pk):
-        equipo=self.get_object(pk)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk):
+        equipo = self.get_object(pk)
         if equipo is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         equipo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# Temp ::::::::::::::::::::::::::::::::::::::::::::
+# Temp :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 from .models import TipoIngreso
 from .serializers import TipoIngresoSerializer
 
+
 class ListTipoIngresoApiView(APIView):
-    allowed_methods = ['GET']
+    allowed_methods = ["GET"]
 
     def get(self, request):
         tipo_ingreso = TipoIngreso.objects.all()
@@ -69,21 +89,22 @@ class ListTipoIngresoApiView(APIView):
 from .models import Estado
 from .serializers import EstadoSerializer
 
+
 class ListEstadosApiView(APIView):
-    allowed_methods = ['GET']
+    allowed_methods = ["GET"]
 
     def get(self, request):
         estados = Estado.objects.all()
         serializer = EstadoSerializer(estados, many=True)
         return Response(serializer.data)
-    
 
 
 from .models import Ubicacion
 from .serializers import UbicacionSerializer
 
+
 class ListUbicacionesApiView(APIView):
-    allowed_methods = ['GET']
+    allowed_methods = ["GET"]
 
     def get(self, request):
         ubicaciones = Ubicacion.objects.all()
